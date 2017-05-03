c29b01bd406a17c5ccc350508278ee38ea3245cd
car <c_chow568@yahoo.ca>
2017-05-03 11:42:41 -0700

    Signup form

--- coffeerun/urls.py
+++ coffeerun/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
   url(r'^admin/', admin.site.urls),
+  url(r'^signup/$', TemplateView.as_view(template_name='signup.html')),
   url(r'^', include('django.contrib.auth.urls')),
   url(r'^$', TemplateView.as_view(template_name='index.html')),
 ] 
